ef147fd060ff2bfbcebe8f311d90f70e91874140
Neurofibromin <125222560+Neurofibromin@users.noreply.github.com>
2025-10-11 10:21:54 +0200

    Move IconConverter to View from ViewModel

--- bookmark-dlp/ViewModels/DownloadingViewModel.cs
+++ bookmark-dlp/ViewModels/DownloadingViewModel.cs
@@ -17,7 +17,7 @@ namespace bookmark_dlp.ViewModels;
 
 public partial class DownloadingViewModel : ViewModelBase
 {
-    private static IconConverter? _iconConverter;
+    
     [ObservableProperty] private SettingsStruct _activeSettings;
     [ObservableProperty] private string? _fileSource;
     private List<Folderclass>? _folders;
@@ -83,95 +83,6 @@ public partial class DownloadingViewModel : ViewModelBase
         TreeSource = CreateTreeSource();
         return true;
     }
-
-    #region IconConverter
-
-    private class IconConverter : IMultiValueConverter
-    {
-        private readonly StreamGeometry _folderCollapsed;
-        private readonly StreamGeometry _folderExpanded;
-
-        public IconConverter(StreamGeometry folderExpanded, StreamGeometry folderCollapsed)
-        {
-            _folderExpanded = folderExpanded;
-            _folderCollapsed = folderCollapsed;
-        }
-
-        public object? Convert(IList<object?>? values, Type targetType, object? parameter, CultureInfo culture)
-        {
-            if (values is null || values.Count < 1)
-                return null;
-            if (values[0] is bool isExpanded) return isExpanded ? _folderExpanded : _folderCollapsed;
-            return null;
-        }
-    }
-
-    /// <summary>
-    ///     Implements IconConverter for folder icons of folderIconopen and folderIconopen
-    /// </summary>
-    public static IMultiValueConverter FileIconConverter
-    {
-        get
-        {
-            // Logger.LogVerbose("Getting FileIconConverter", Logger.Verbosity.Trace);
-            if (_iconConverter is null)
-            {
-                Logger.LogVerbose("FileIconConverter is NULL", Logger.Verbosity.Trace);
-                bool a = Application.Current!.Styles.TryGetResource("folder_regular",
-                    Application.Current.ActualThemeVariant, out object? folderIconregular);
-                bool b = Application.Current.Styles.TryGetResource("folder_open_regular",
-                    Application.Current.ActualThemeVariant, out object? folderIconopen);
-
-                if (a && b && folderIconopen is StreamGeometry openFolderGeometry &&
-                    folderIconregular is StreamGeometry regularFolderGeometry)
-                {
-                    Logger.LogVerbose("FileIconConverter found", Logger.Verbosity.Trace);
-                    _iconConverter = new IconConverter(openFolderGeometry, regularFolderGeometry);
-                }
-                else
-                {
-                    if (a || b)
-                    {
-                        if (folderIconopen is StreamGeometry openFolderGeometry2)
-                        {
-                            Logger.LogVerbose("Only folderIconopen found", Logger.Verbosity.Error);
-                            _iconConverter = new IconConverter(openFolderGeometry2, new StreamGeometry());
-                        }
-                        else if (folderIconregular is StreamGeometry regularFolderGeometry2)
-                        {
-                            Logger.LogVerbose("Only folderIconregular found", Logger.Verbosity.Error);
-                            _iconConverter = new IconConverter(new StreamGeometry(), regularFolderGeometry2);
-                        }
-                        else
-                        {
-                            Logger.LogVerbose("Failed to load folder icons. Using default values.",
-                                Logger.Verbosity.Error);
-                            _iconConverter =
-                                new IconConverter(new StreamGeometry(), new StreamGeometry()); // Provide default values
-                        }
-                    }
-                    else
-                    {
-                        Logger.LogVerbose("Failed to load folder icons. Using default values.", Logger.Verbosity.Error);
-                        _iconConverter =
-                            new IconConverter(new StreamGeometry(), new StreamGeometry()); // Provide default values
-                    }
-                }
-
-
-                /*if (a && b)
-                    Logger.LogVerbose("FileIconConverter found", Logger.Verbosity.Trace);
-                else
-                    Logger.LogVerbose("FileIconConverter not found", Logger.Verbosity.Error);
-                Logger.LogVerbose("Found folder icon resources.", Logger.Verbosity.Trace);
-                _iconConverter = new IconConverter((StreamGeometry) folderIconopen, (StreamGeometry) folderIconregular);*/
-            }
-
-            return _iconConverter;
-        }
-    }
-
-    #endregion
 
     #region TreeDataGrid
 
